12f729d023b121e2a09b357e0917a026e89db1fe
Gajanand Khatriya <khatriya.gajanand@webseeder.tech>
2025-06-04 14:20:46 +0530

    BrandForm and BrandList Component updated to add image upload and show functionality and

--- src/components/BrandForm.jsx
+++ src/components/BrandForm.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import Quill from "quill";
+import { FaTrash } from "react-icons/fa";
+import { BASE_URL } from "../redux/constants";
 
 const BrandForm = ({
   value,
@@ -11,9 +13,12 @@ const BrandForm = ({
   handleSubmit,
   buttonText = "Submit",
   handleDelete,
+  image,
+  setImage
 }) => {
   const quillRef = useRef(null);
 
+  console.log(aboutTheBrand)
   useEffect(() => {
     const quill = quillRef.current?.getEditor();
     if (!quill) return;
@@ -56,6 +61,42 @@ const BrandForm = ({
             className="w-full mb-3"
           />
         </div>
+
+                {/* Upload Image with dashed border */}
+        <div className="space-y-1">
+          <label className="text-sm font-medium text-gray-700 dark:text-gray-300">
+            Upload Image
+          </label>
+          <div className="relative flex items-center justify-center border-2 border-dashed border-gray-400 dark:border-gray-600 rounded-md p-4 cursor-pointer hover:border-gray-500 transition-all h-[150px]">
+            <input
+              type="file"
+              accept="image/*"
+              className="opacity-0 absolute inset-0 w-full h-full cursor-pointer"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
+            <p className="text-sm text-gray-600 dark:text-gray-300 z-10 pointer-events-none">
+              Drag & drop or click to upload
+            </p>
+          </div>
+        </div>
+
+                {/* Preview */}
+        {image && (
+          <div className="relative mt-4 w-32 h-32">
+            <img
+              src={typeof image === "string" ? `${BASE_URL}/${image}` : URL.createObjectURL(image)}
+              alt="Category Preview"
+              className="w-full h-full object-cover rounded-lg border shadow-md ring-1 ring-gray-300"
+            />
+            <button
+              type="button"
+              onClick={() => setImage(null)}
+              className="absolute top-1 right-1 bg-white rounded-full p-1 text-red-600 hover:text-red-800 hover:bg-gray-100 shadow"
+            >
+              <FaTrash size={16} />
+            </button>
+          </div>
+        )}
 
         <div className="flex flex-col sm:flex-row sm:space-x-3 space-y-3 sm:space-y-0 justify-center">
           <button
